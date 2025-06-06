dc1ab3032daab46c06069d7588648b0d3f46e318
ToDiii <ToDiii@users.noreply.github.com>
2025-06-06 21:44:50 +0200

    Add IndexPage component

---
+++ frontend/src/pages/IndexPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from '@usetrmnl/framework'
+
+function IndexPage() {
+  return (
+    <View>
+      <Text>Hello TRMNL!</Text>
+    </View>
+  )
+}
+
+export default IndexPage
